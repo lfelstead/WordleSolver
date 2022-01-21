8ef330bd674770a45a20e4c9658bf300dedcbb7c
lfelstead <57783923+lfelstead@users.noreply.github.com>
2022-01-21 11:24:42 +0800

    Added guess evaluation function

--- wordleweb.py
+++ wordleweb.py
@@ -37,7 +37,7 @@ rows = board.find_elements_by_tag_name("game-row")
 # from website HTML gets the attributes of each row of guesses 
 # returns array with correct letters and placement, and two strings for present and absent letters
 def getfeedback(rows):
-    correct, present, absent = [], "", ""
+    correct, present, absent = [], [], ""
     for row in rows:
         tiles = driver.execute_script("return arguments[0].shadowRoot.querySelectorAll('.row')", row)
         for tile in tiles:
@@ -51,7 +51,7 @@ def getfeedback(rows):
                     if evaluation == "correct" and (letter, count) not in correct:
                         correct.append((letter, count))
                     elif evaluation == "present" and letter not in present:
-                        present += letter
+                        present.append((letter, count))
                     elif evaluation == "absent" and letter not in absent:
                         absent += letter
 
@@ -65,34 +65,56 @@ def possibleguesses(answers, correct, present, absent):
     for word in answers:
         match = True
         for letter, placement in correct:
-            if word[placement] != letter:
-                match = False
-        for letter in present:
-            if letter not in word:
-                match = False
+            if word[placement] != letter: match = False
+        for letter, placement in present:
+            if letter not in word or word[placement] == letter: match = False
         for letter in absent:
-            if letter in word:
-                match = False
+            if letter in word and letter not in present and letter not in correct: match = False
         
         if match:
             possible.append(word)
 
     return possible
 
+def findbestguess(answers, correct, present, absent):
+        guesslist = possibleguesses(answers, correct, present, absent)
+        freqdict = {}
+        listlength= len(guesslist)
+        for letter in "qwertyuiopasdfghjklzxcvbnm":
+            letteroccurance = sum(s.count(letter) for s in guesslist)
+            if letteroccurance>listlength: letteroccurance = 0
+            elif letteroccurance>listlength/2: letteroccurance = listlength-letteroccurance
+            print("letter {0} occurance:{1}".format(letter, letteroccurance))
+            freqdict[letter] = letteroccurance
+        print(freqdict)
+
+        maxpoints = -1
+        bestword = ""
+        for word in guesslist:
+            points  = 0
+            for letter in word:
+                points += freqdict[letter]
+            print("word:{0}, points:{1}".format(word, points))
+
+            if points > maxpoints: 
+                maxpoints = points
+                bestword = word
+        print(bestword)
+        return bestword
 
 correct, present, absent = getfeedback(rows)
 
 while len(correct) != 5: 
     print("correct: {0}, present: {1}, absent: {2}".format(correct, present, absent))
-    guesslist = possibleguesses(answers, correct, present, absent)
+    #guesslist = possibleguesses(answers, correct, present, absent)
+    #print(guesslist)
+    #choice = random.choice(guesslist)
 
-    # ***** need to write an evaluation function for this rather than a random choice ******
-    randomchoice = random.choice(guesslist)
+    choice = findbestguess(answers, correct, present, absent)
 
-    guess = []
-    for letter in randomchoice:
-        guess.append(letter)
+    guess = [letter for letter in choice]
 
+    time.sleep(2)
     Elem.send_keys(guess)
     Elem.send_keys(Keys.ENTER)
 
