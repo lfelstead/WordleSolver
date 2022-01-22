c01488ba25d15c46ab3517914802033bf88aa291
lfelstead <57783923+lfelstead@users.noreply.github.com>
2022-01-22 21:43:50 +0800

    now chooses starting word and bug fixes
    - reordered code to choose starting word instead of preprogrammed "roate" 
    - fixed problems that occurred when word with duplicated letters were used 
    - small change to evaluation function so that word with repeating letters are no longer priorised.

--- wordleweb.py
+++ wordleweb.py
@@ -21,43 +21,6 @@ Elem.click()
 
 time.sleep(2)
 
-Elem.send_keys(['r','o','a','t','e'])
-Elem.send_keys(Keys.ENTER)
-time.sleep(2)
-
-
-host = driver.find_element_by_tag_name("game-app")
-game = driver.execute_script("return arguments[0].shadowRoot.getElementById('game')", host)
-
-boardcontainer = game.find_element_by_id("board-container")
-board = boardcontainer.find_element_by_id("board")
-
-rows = board.find_elements_by_tag_name("game-row")
-
-# from website HTML gets the attributes of each row of guesses 
-# returns array with correct letters and placement, and two strings for present and absent letters
-def getfeedback(rows):
-    correct, present, absent = [], [], ""
-    for row in rows:
-        tiles = driver.execute_script("return arguments[0].shadowRoot.querySelectorAll('.row')", row)
-        for tile in tiles:
-            gametile = tile.find_elements_by_tag_name("game-tile")
-            count = 0
-            for t in gametile:
-                letter = driver.execute_script("return arguments[0].getAttribute('letter');",t)
-                if letter != None:
-                    evaluation = driver.execute_script("return arguments[0].getAttribute('evaluation');",t)
-
-                    if evaluation == "correct" and (letter, count) not in correct:
-                        correct.append((letter, count))
-                    elif evaluation == "present" and letter not in present:
-                        present.append((letter, count))
-                    elif evaluation == "absent" and letter not in absent:
-                        absent += letter
-
-                count += 1
-    return (correct, present, absent)
-
 # returns array containing all words that align with the known information
 def possibleguesses(answers, correct, present, absent):
     possible = []
@@ -69,7 +32,7 @@ def possibleguesses(answers, correct, present, absent):
         for letter, placement in present:
             if letter not in word or word[placement] == letter: match = False
         for letter in absent:
-            if letter in word and letter not in present and letter not in correct: match = False
+            if letter in word: match = False
         
         if match:
             possible.append(word)
@@ -92,7 +55,8 @@ def findbestguess(answers, correct, present, absent):
         bestword = ""
         for word in guesslist:
             points  = 0
-            for letter in word:
+            nonrepeat = "".join(set(word))
+            for letter in nonrepeat:
                 points += freqdict[letter]
             print("word:{0}, points:{1}".format(word, points))
 
@@ -102,15 +66,63 @@ def findbestguess(answers, correct, present, absent):
         print(bestword)
         return bestword
 
+choice = findbestguess(answers, [], [], "")
+guess = [letter for letter in choice]
+
+#Elem.send_keys(['r','o','a','t','e'])
+Elem.send_keys(guess)
+Elem.send_keys(Keys.ENTER)
+time.sleep(2)
+
+
+host = driver.find_element_by_tag_name("game-app")
+game = driver.execute_script("return arguments[0].shadowRoot.getElementById('game')", host)
+
+boardcontainer = game.find_element_by_id("board-container")
+board = boardcontainer.find_element_by_id("board")
+
+rows = board.find_elements_by_tag_name("game-row")
+
+# from website HTML gets the attributes of each row of guesses 
+# returns array with correct letters and placement, and two strings for present and absent letters
+def getfeedback(rows):
+    correct, present, absent = [], [], ""
+    tcorrect = ""
+    for row in rows:
+        tiles = driver.execute_script("return arguments[0].shadowRoot.querySelectorAll('.row')", row)
+        for tile in tiles:
+            gametile = tile.find_elements_by_tag_name("game-tile")
+            count = 0
+            for t in gametile:
+                letter = driver.execute_script("return arguments[0].getAttribute('letter');",t)
+                if letter != None:
+                    evaluation = driver.execute_script("return arguments[0].getAttribute('evaluation');",t)
+
+                    if evaluation == "correct" and (letter, count) not in correct:
+                        correct.append((letter, count))
+                    elif evaluation == "present" and (letter, count) not in present:
+                        present.append((letter, count))
+                    elif evaluation == "absent":
+                        absent += letter
+
+                count += 1
+    presentletters = [a_tuple[0] for a_tuple in correct]
+    presentletters += [a_tuple[0] for a_tuple in present]
+    for letter in presentletters:
+        if letter in absent:
+            absent = absent.replace(letter, "")
+
+    return (correct, present, absent)
+
 correct, present, absent = getfeedback(rows)
 
 while len(correct) != 5: 
     print("correct: {0}, present: {1}, absent: {2}".format(correct, present, absent))
-    #guesslist = possibleguesses(answers, correct, present, absent)
-    #print(guesslist)
-    #choice = random.choice(guesslist)
+    guesslist = possibleguesses(answers, correct, present, absent)
+    print(guesslist)
+    choice = random.choice(guesslist)
 
-    choice = findbestguess(answers, correct, present, absent)
+    #choice = findbestguess(answers, correct, present, absent)
 
     guess = [letter for letter in choice]
 
